3889ec9e41c0397769be2b4a986cbdcab5b9009a
lsd-arf <vlad_lcd@mail.ru>
2021-02-10 17:57:59 +0300

    Задание 12. Построить предикат, который проверяет, является ли X дядей Y. Построить предикат, который выводит всех дядей X.

--- lab1/base.pl
+++ lab1/base.pl
@@ -96,3 +96,6 @@ grand_dats(X):-grand_da(Y,X),c_write(Y),fail.
 grand_pa_and_son(X,Y):-( grand_pa(X,Y),man(Y) )|( grand_pa(Y,X),man(X) ).
 
 grand_ma_and_da(X,Y):-( grand_da(X,Y),woman(Y) )|( grand_da(Y,X),woman(X) ).
+
+uncle(X,Y):-brother(X,A),parent(A,Y).
+uncles(X):-uncle(Y,X),c_write(Y),fail.
