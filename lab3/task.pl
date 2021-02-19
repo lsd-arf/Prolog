8a301fcd4fcbf2d11947b230aeca74499678147d
lsd-arf <vlad_lcd@mail.ru>
2021-02-20 02:20:22 +0300

    Задание 4 Реализовать предикат fact(N,X), где X - это факториал первого аргумента с помощью рекурсии вниз.

--- lab3/task.pl
+++ lab3/task.pl
@@ -7,3 +7,7 @@ max(_,_,U,Z):- Z is U.
 % рекурсия вверх
 fact(0,1):- !.
 fact(N,X):- N1 is N-1, fact(N1, X1), X is X1*N.
+% рекурсия вниз
+fact(0,CurX,CurX):- !.
+fact(N,CurX,X):- CurX1 is CurX*N, N1 is N-1, fact(N1, CurX1, X).
+factorial(N,X):- fact(N,1,X).
