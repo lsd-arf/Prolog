4cf74c97798acb73da6c9b3a9704e486a8b27615
lsd-arf <vlad_lcd@mail.ru>
2021-02-20 03:05:11 +0300

    Задание 5 Реализовать предикат fib(N,X), где X - число Фибоначчи с номером N, причем 1 и 2 элемент равны 1 с помощью рекурсии вверх.

--- lab3/task.pl
+++ lab3/task.pl
@@ -1,13 +1,19 @@
-max(X,Y,Z):- X>=Y, Z is X, !.
-max(_,Y,Z):- Z is Y.
+max(X, Y, Z):- X >= Y, Z is X, !.
+max(_, Y, Z):- Z is Y.
 
-max(X,Y,U,Z):- X>=Y, X>=U, Z is X, !.
-max(_,Y,U,Z):- Y>=U, Z is Y, !.
-max(_,_,U,Z):- Z is U.
+max(X, Y, U, Z):- X >= Y, X >= U, Z is X, !.
+max(_, Y, U, Z):- Y >= U, Z is Y, !.
+max(_, _, U, Z):- Z is U.
+
 % рекурсия вверх
-fact(0,1):- !.
-fact(N,X):- N1 is N-1, fact(N1, X1), X is X1*N.
+fact(0, 1):- !.
+fact(N, X):- N1 is N - 1, fact(N1, X1), X is X1 * N.
+
 % рекурсия вниз
-fact(0,CurX,CurX):- !.
-fact(N,CurX,X):- CurX1 is CurX*N, N1 is N-1, fact(N1, CurX1, X).
-factorial(N,X):- fact(N,1,X).
+fact(0, CurX, CurX):- !.
+fact(N, CurX, X):- CurX1 is CurX * N, N1 is N - 1, fact(N1, CurX1, X).
+factorial(N, X):- fact(N, 1, X).
+
+fib(1, 1):- !.
+fib(2, 1):- !.
+fib(N, X):- N1 is N - 1, N2 is N - 2, fib(N1, X1), fib(N2, X2), X is X1 + X2.
