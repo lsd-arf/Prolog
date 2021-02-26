1f4193e78740e3d8a6da2ec036871f04ca7323f2
lsd-arf <vlad_lcd@mail.ru>
2021-02-27 01:47:17 +0300

    Задание 2 Реализовать предикат sum_list_down(+List, ?Summ), который проверяет, является ли Summ суммой элементов списка или записывает в эту переменную сумму элементов. Для построения воспользоваться рекурсией вниз. Реализовать программу, читающую список, считающую сумму элементов и выводящую сумму на экран, с использованием предикатов из задания 1 и построенного предиката sum_list_down /2.

--- lab4/task.pl
+++ lab4/task.pl
@@ -1,15 +1,25 @@
+% Задание 1
+% связываем 2 списка
+append_ls([], X, X).
+append_ls([H|T1], X, [H|T2]) :- append_ls(T1, X, T2).
+
 % читаем список
-read_str(N, L) :- read_str(N, 0, [], L), !.
-read_str(N, N, L, L) :- !.
-read_str(N, CurN, CurL, L) :- 
+read_ls(N, L) :- read_ls(N, 0, [], L), !.
+read_ls(N, N, L, L) :- !.
+read_ls(N, CurN, CurL, L) :- 
   CurN1 is CurN + 1, 
   read(X), 
-  append(CurL, [X], CurL1), 
-  read_str(N, CurN1, CurL1, L).
+  append_ls(CurL, [X], CurL1), 
+  read_ls(N, CurN1, CurL1, L).
 
 % выводим список
-write_list([]) :- !.
-write_list([H|T]) :- 
+write_ls([]) :- !.
+write_ls([H|T]) :- 
   write(H), 
   write(" "), 
-  write_list(T).
+  write_ls(T).
+
+% Задание 2
+sum_ls_down([], Sum, Sum) :- !.
+sum_ls_down([H|T], CurSum, Sum) :- CurSum1 is CurSum + H, sum_ls_down(T, CurSum1, Sum).
+sum_ls_down([H|T], Sum) :- sum_ls_down([H|T], 0, Sum).
