9e0d3dd8bf46e678bf522922c7c15a2218558d23
lsd-arf <vlad_lcd@mail.ru>
2021-02-28 16:42:36 +0300

    Задача 12 Дан целочисленный массив. Необходимо переставить в обратном порядке элементы массива, расположенные между его минимальным и максимальным элементами.

--- lab4/task.pl
+++ lab4/task.pl
@@ -21,35 +21,65 @@ write_ls([H|T]) :-
 
 % Задание 2
 sum_ls_down([], Sum, Sum) :- !.
-sum_ls_down([H|T], CurSum, Sum) :- CurSum1 is CurSum + H, sum_ls_down(T, CurSum1, Sum).
+sum_ls_down([H|T], CurSum, Sum) :- 
+  CurSum1 is CurSum + H, 
+  sum_ls_down(T, CurSum1, Sum).
 sum_ls_down([H|T], Sum) :- sum_ls_down([H|T], 0, Sum).
 
 % Задание 3
 sum_ls_up([], 0) :- !.
-sum_ls_up([H|T], Sum) :- sum_ls_up(T, Sum1), Sum is Sum1 + H.
+sum_ls_up([H|T], Sum) :- 
+  sum_ls_up(T, Sum1), 
+  Sum is Sum1 + H.
 
 % Задание 4
-ls_num_el([H], El, CurNum, Num) :- ((H = El) -> Num is CurNum + 1; write("Such element isn\'t found")), !.
-ls_num_el([H|T], El, CurNum, Num) :- CurNum1 is CurNum + 1, ((El = H) -> Num is CurNum1; ls_num_el(T, El, CurNum1, Num)).
+ls_num_el([H], El, CurNum, Num) :- 
+  ((H = El) -> 
+  Num is CurNum + 1; 
+  write("Such element isn\'t found")), !.
+ls_num_el([H|T], El, CurNum, Num) :- 
+  CurNum1 is CurNum + 1, 
+  ((El = H) -> 
+  Num is CurNum1; 
+  ls_num_el(T, El, CurNum1, Num)).
 ls_num_el([H|T], El, Num) :- ls_num_el([H|T], El, 0, Num).
 
 % Задание 5
 % номер элемента в списке
 ls_el_at_num([], _, _, _) :- write("Such element isn\'t found"), !.
-ls_el_at_num([_], CurNum, Num, _) :- CurNum1 is CurNum + 1, CurNum1 < Num, write("Such element isn\'t found"), !.
-ls_el_at_num([H|T], CurNum, Num, El) :- CurNum1 is CurNum + 1, ((CurNum1 is Num) -> El is H; ls_el_at_num(T, CurNum1, Num, El)).
+ls_el_at_num([_], CurNum, Num, _) :- 
+  CurNum1 is CurNum + 1, 
+  CurNum1 < Num, 
+  write("Such element isn\'t found"), !.
+ls_el_at_num([H|T], CurNum, Num, El) :- 
+  CurNum1 is CurNum + 1, 
+  ((CurNum1 is Num) -> 
+  El is H; 
+  ls_el_at_num(T, CurNum1, Num, El)).
 ls_el_at_num([H|T], Num, El) :- ls_el_at_num([H|T], 0, Num, El).
 
 % Задание 6
-min_ls_up([H], CurMin, Min) :- (H < CurMin -> Min is H; Min is CurMin), !.
-min_ls_up([H|T], CurMin, Min) :- (H < CurMin -> CurMin1 is H; CurMin1 is CurMin), min_ls_up(T, CurMin1, Min), Min is CurMin1.
+min_ls_up([H], CurMin, Min) :- 
+  (H < CurMin -> 
+  Min is H; 
+  Min is CurMin), !.
+min_ls_up([H|T], CurMin, Min) :- 
+  (H < CurMin -> 
+  CurMin1 is H; 
+  CurMin1 is CurMin), 
+  min_ls_up(T, CurMin1, Min), 
+  Min is CurMin1.
 min_ls_up([], _) :- write("List isn't exist"), fail, !.
 min_ls_up([H], Min) :- Min is H, !.
 min_ls_up([H|T], Min) :- min_ls_up(T, H, Min).
 
 % Задание 7
 min_ls_down([], Min, Min) :- !.
-min_ls_down([H|T], CurMin, Min) :- (H < CurMin -> CurMin1 is H; CurMin1 is CurMin), min_ls_down(T, CurMin1, Min).
+min_ls_down([H|T], CurMin, Min) :- 
+  (H < CurMin -> 
+  CurMin1 is H; 
+  CurMin1 is CurMin), 
+  min_ls_down(T, CurMin1, Min).
 min_ls_down([H|T], Min) :- min_ls_down(T, H, Min).
 
 % Задание 8
@@ -76,38 +106,121 @@ rev_ls(List, InvList) :- rev_ls(List, [], InvList).
 
 % Задание 11
 p([], _) :- !.
-p([SubH|SubT], [H|T]) :- (SubH = H -> p(SubT, T); p([SubH|SubT], T)).
+p([SubH|SubT], [H|T]) :- 
+  (SubH = H -> 
+  p(SubT, T); 
+  p([SubH|SubT], T)).
 
 % Задание 12
 rm_el([_|T], CurList, Num, Num, NewList) :- append_ls(CurList, T, NewList), !.
-rm_el([H|T], CurList, CurNum, Num, NewList) :- append_ls(CurList, [H], CurList1), CurNum1 is CurNum + 1, rm_el(T, CurList1, CurNum1, Num, NewList).
+rm_el([H|T], CurList, CurNum, Num, NewList) :- 
+  append_ls(CurList, [H], CurList1), 
+  CurNum1 is CurNum + 1, 
+  rm_el(T, CurList1, CurNum1, Num, NewList).
 rm_el(List, Num, NewList) :- rm_el(List, [], 1, Num, NewList).
 
 % Задание 13
 % удаление одинаковых с заданным элементов
 rm_equals(List, El, List) :- not(in_ls(List, El)), !.
-rm_equals(List, El, NewList) :- ls_num_el(List, El, Num), rm_el(List, Num, List1), rm_equals(List1, El, NewList).
+rm_equals(List, El, NewList) :- 
+  ls_num_el(List, El, Num), 
+  rm_el(List, Num, List1), 
+  rm_equals(List1, El, NewList).
 
 % Задание 14
 uni_els([]) :- !.
-uni_els([H|T]) :- not(in_ls(T, H)), uni_els(T).
+uni_els([H|T]) :- 
+  not(in_ls(T, H)), 
+  uni_els(T).
 
 % Задание 15
 uni_list([], []) :- !.
-uni_list([H|T], List) :- rm_equals(T, H, RmList), uni_list(RmList, List1), append_ls([H], List1, List).
+uni_list([H|T], List) :- 
+  rm_equals(T, H, RmList), 
+  uni_list(RmList, List1), 
+  append_ls([H], List1, List).
 
 % Задание 16
 count_equals([], _, Count, Count) :- !.
-count_equals([H|T], El, CurCount, Count) :- (H = El -> CurCount1 is CurCount + 1; CurCount1 is CurCount), count_equals(T, El, CurCount1, Count).
+count_equals([H|T], El, CurCount, Count) :- 
+  (H = El -> 
+  CurCount1 is CurCount + 1; 
+  CurCount1 is CurCount), 
+  count_equals(T, El, CurCount1, Count).
 count_equals(List, El, Count) :- count_equals(List, El, 0, Count).
 
 % Задание 17
 % количество элементов в списке
 count_els([], Count, Count) :- !.
-count_els([_|T], CurCount, Count) :- CurCount1 is CurCount + 1, count_els(T, CurCount1, Count).
+count_els([_|T], CurCount, Count) :- 
+  CurCount1 is CurCount + 1, 
+  count_els(T, CurCount1, Count).
 count_els(List, Count) :- count_els(List, 0, Count).
 
 % Задание 18, Вариант 12
 % Задача 11
-find_el1([H|T], El) :- rm_equals([H|T], H, [NewH|NewT]), count_els([NewH|NewT], Count), (Count = 1 -> El is NewH; El is H).
-find_el2([H|T], El) :- (in_ls(T, H) -> find_el2(T, El); El is H).
+find_el1([H|T], El) :- 
+  rm_equals([H|T], H, [NewH|NewT]), 
+  count_els([NewH|NewT], Count), 
+  (Count = 1 -> 
+  El is NewH; 
+  El is H).
+find_el2([H|T], El) :- 
+  (in_ls(T, H) -> 
+  find_el2(T, El); 
+  El is H).
+
+% Задача 12
+% максимальный элемент
+max_ls_down([], Max, Max) :- !.
+max_ls_down([H|T], CurMax, Max) :- 
+  (H > CurMax -> 
+  CurMax1 is H; 
+  CurMax1 is CurMax), 
+  max_ls_down(T, CurMax1, Max).
+max_ls_down([H|T], Max) :- max_ls_down(T, H, Max).
+
+% получить список до номера (вместе с номером)
+get_ls_before(_, Num, Num, List, List) :- !.
+get_ls_before([H|T], CurNum, Num, CurList, NewList) :- 
+  append_ls(CurList, [H], CurList1),
+  CurNum1 is CurNum + 1,
+  get_ls_before(T, CurNum1, Num, CurList1, NewList).
+get_ls_before(List, Num, NewList) :- get_ls_before(List, 0, Num, [], NewList).
+
+% получить список после номера (вместе с номером)
+get_ls_after(List, Num, ListAfter) :- 
+  Num1 is Num - 1, 
+  get_ls_before(List, Num1, ListBefore), 
+  append_ls(ListBefore, ListAfter, List).
+
+% получить список между номерами (без них)
+get_ls_between(List, Num1, Num2, ListBetween) :- 
+  get_ls_before(List, Num1, ListBefore),
+  append_ls(ListBefore, ListAfter, List),
+  Num22 is Num2 - Num1 - 1,
+  get_ls_before(ListAfter, Num22, ListBetween).
+
+rev_between(List, NewList) :- 
+  min_ls_down(List, Min),
+  max_ls_down(List, Max),
+  ls_num_el(List, Min, Nmin),
+  ls_num_el(List, Max, Nmax),
+  (
+  Nmin = Nmax ->
+  append_ls(List, [], NewList);
+  (
+  Nmin < Nmax ->
+  get_ls_before(List, Nmin, ListBefore),
+  get_ls_after(List, Nmax, ListAfter),
+  get_ls_between(List, Nmin, Nmax, ListBetween);
+  (
+  get_ls_before(List, Nmax, ListBefore),
+  get_ls_after(List, Nmin, ListAfter),
+  get_ls_between(List, Nmax, Nmin, ListBetween)
+  )
+  ),
+  rev_ls(ListBetween, RevList),
+  append_ls(ListBefore, RevList, ListBB),
+  append_ls(ListBB, ListAfter, NewList)
+  ).
