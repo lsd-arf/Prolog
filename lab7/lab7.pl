3030a1f81b2c71caffb9a7bfdff580c3318e50d6
lsd-arf <vlad_lcd@mail.ru>
2021-03-10 16:38:01 +0300

    Задание 2 Дана строка. Найти количество слов.

--- lab7/lab7.pl
+++ lab7/lab7.pl
@@ -25,12 +25,21 @@ r_str_nofix(X, A, B) :-
   get0(X1),
   r_str_nofix(X1, A, B1).
 
-% количество элементов в строке
+% количество элементов в списке
 count_els([], Count, Count) :- !.
 count_els([_|T], CurCount, Count) :- 
   CurCount1 is CurCount + 1, 
   count_els(T, CurCount1, Count).
 count_els(List, Count) :- count_els(List, 0, Count).
+
+% количество символов в строке
+count_chrs([], Count, Count) :- !.
+count_chrs([H|T], CurCount, Count) :- 
+  (H = 32 ->
+  CurCount1 is CurCount;
+  CurCount1 is CurCount + 1), 
+  count_chrs(T, CurCount1, Count).
+count_chrs(List, Count) :- count_chrs(List, 0, Count).
 
 % Задание 1
 task1 :-
@@ -44,3 +53,29 @@ task1 :-
   write("Elements => "),
   count_els(S, Count),
   write(Count).
+
+% Задание 2
+% получаем строку без первых пробелов
+list_nofirstspaces([], []) :- !.
+list_nofirstspaces([H|T], [H|T]) :- H \= 32, !.
+list_nofirstspaces([_|T], NewList) :- list_nofirstspaces(T, NewList).
+
+% получаем слово до первого пробела (работает, если в начале нет пробелов)
+firstword([], Word, Word) :- !.
+firstword([H|_], Word, Word) :- H = 32, !.
+firstword([H|T], CurWord, NewWord) :-
+  append(CurWord, [H], CurWord1),
+  firstword(T, CurWord1, NewWord).
+firstword(List, Word) :- firstword(List, [], Word).
+
+% считаем количество слов
+count_words([], CountWords, CountWords) :- !.
+count_words(List, CurCountWords, CountWords) :-
+  list_nofirstspaces(List, ListNoFirstSpaces),
+  (ListNoFirstSpaces \= [] ->
+  (firstword(ListNoFirstSpaces, FirstWord),
+  append(FirstWord, ListNoFirstWord, ListNoFirstSpaces),
+  CurCountWords1 is CurCountWords + 1);
+  (ListNoFirstWord = [],
+  CurCountWords1 is CurCountWords)),
+  count_words(ListNoFirstWord, CurCountWords1, CountWords).
