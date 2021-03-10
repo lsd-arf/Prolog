ce68e30797d76f5dbdd7faf4781d191e89032baf
lsd-arf <vlad_lcd@mail.ru>
2021-03-10 17:56:54 +0300

    Задание 3 Дана строка, определить самое частое слово.

--- lab7/lab7.pl
+++ lab7/lab7.pl
@@ -69,13 +69,74 @@ firstword([H|T], CurWord, NewWord) :-
 firstword(List, Word) :- firstword(List, [], Word).
 
 % считаем количество слов
-count_words([], CountWords, CountWords) :- !.
-count_words(List, CurCountWords, CountWords) :-
+% убираем первые пробелы
+% если новый список не пустой, убираем слово, считаем + 1
+% иначе слов не осталось, ничего не считаем
+% кидаем новый ListNoFirstWord или с (пробелами + словами), или пустой
+count_words([], Count, Count) :- !.
+count_words(List, CurCount, Count) :-
   list_nofirstspaces(List, ListNoFirstSpaces),
   (ListNoFirstSpaces \= [] ->
   (firstword(ListNoFirstSpaces, FirstWord),
   append(FirstWord, ListNoFirstWord, ListNoFirstSpaces),
-  CurCountWords1 is CurCountWords + 1);
+  CurCount1 is CurCount + 1);
   (ListNoFirstWord = [],
-  CurCountWords1 is CurCountWords)),
-  count_words(ListNoFirstWord, CurCountWords1, CountWords).
+  CurCount1 is CurCount)),
+  count_words(ListNoFirstWord, CurCount1, Count).
+count_words(List, Count) :- count_words(List, 0, Count).
+
+task2 :-
+  write("Str -> "),
+  read_str_nofix(S),
+  write("Count of words => "),
+  count_words(S, Count),
+  write(Count).
+
+% Задание 3
+% получаем первое слово несмотря на пробелы
+firstword_nfs(List, Word) :-
+  list_nofirstspaces(List, ListNFS),
+  firstword(ListNFS, Word).
+
+% получаем список слов
+list_of_words(List, LW, LW) :-
+  list_nofirstspaces(List, ListNFS),
+  ListNFS = [], !.
+list_of_words(Str, CurLW, LW) :-
+  list_nofirstspaces(Str, StrNFS),
+  firstword(StrNFS, Word),
+  append(Word, StrNoWord, StrNFS),
+  append(CurLW, [Word], CurLW1),
+  list_of_words(StrNoWord, CurLW1, LW).
+list_of_words(Str, LW) :- list_of_words(Str, [], LW).
+
+% одинаковые ли списки
+lists_equals([], []) :- !.
+lists_equals([H|T1], [H|T2]) :- lists_equals(T1, T2).
+
+% число раз, которое повторяется заданный элемент в списке
+count_equals([], _, Count, Count) :- !.
+count_equals([H|T], El, CurCount, Count) :- 
+  (H = El -> 
+  CurCount1 is CurCount + 1; 
+  CurCount1 is CurCount), 
+  count_equals(T, El, CurCount1, Count).
+count_equals(List, El, Count) :- count_equals(List, El, 0, Count).
+
+% слово, которое встречается чаще всего
+regular_word([], _, Word, Word) :- !.
+regular_word([H|T], Count, CurWord, Word) :-
+  count_equals([H|T], H, Count1),
+  (Count1 > Count ->
+  CurWord1 = H;
+  CurWord1 = CurWord),
+  regular_word(T, Count1, CurWord1, Word).
+regular_word(List, Word) :- regular_word(List, 0, [], Word).
+
+task3 :-
+  write("Str -> "),
+  read_str_nofix(S),
+  list_of_words(S, LW),
+  regular_word(LW, Word),
+  write("Regular word => "),
+  write_str(Word).
