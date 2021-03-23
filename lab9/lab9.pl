c5ef56493ad8c7984994f7812876769c3d338fb6
lsd-arf <vlad_lcd@mail.ru>
2021-03-23 18:18:45 +0300

    Задание 1 Дано множество. Построить все размещения с повторениями по k элементов. Дано множество. Построить все перестановки. Дано множество. Построить все размещения по k элементов. Дано множество. Построить все подмножества. Дано множество. Построить все сочетания по k элементов. Дано множество. Построить все сочетания с повторениями.

---
+++ lab9/lab9.pl
@@ -0,0 +1,88 @@
+in_list([El|_], El).
+in_list([_|T], El) :- in_list(T, El).
+
+in_list_exclude([El|T], El, T).
+in_list_exclude([H|T], El, [H|Tail]) :- in_list_exclude(T, El, Tail).
+
+read_str(A, N) :-
+  get0(X),
+  r_str(X, A, [], N, 0).
+r_str(10, A, A, N, N) :- !.
+r_str(X, A, B, N, K) :-
+  K1 is K + 1,
+  append(B, [X], B1),
+  get0(X1),
+  r_str(X1, A, B1, N, K1).
+
+write_str([]) :- !.
+write_str([H|Tail]) :- put(H), write_str(Tail).
+
+% все размещения с повторениями по k элементов
+build_all_razm_p :-
+  read_str(A, _),
+  read(K),
+  b_a_rp(A, K, []).
+		
+b_a_rp(_, 0, Perm1) :- write_str(Perm1), nl, !, fail.
+b_a_rp(A, N, Perm) :-
+  in_list(A, El),
+  N1 is N - 1,
+  b_a_rp(A, N1, [El|Perm]).
+
+% все перестановки
+build_all_perm:-
+  read_str(A, _),
+  b_a_p(A, []).
+
+b_a_p([], Perm1) :- write_str(Perm1), nl, !, fail.
+b_a_p(A, Perm) :-
+  in_list_exclude(A, El, A1),
+  b_a_p(A1, [El|Perm]).
+
+% все размещения по k элементов
+build_all_razm:-
+  read_str(A, _),
+  read(K),
+  b_a_r(A, K, []).
+
+b_a_r(_, 0, Perm1) :- write_str(Perm1), nl, !, fail.
+b_a_r(A, N, Perm) :-
+  in_list_exclude(A, El, A1),
+  N1 is N - 1,
+  b_a_r(A1, N1, [El|Perm]).
+
+% все подмножества
+sub_set([], []).
+sub_set([H|Sub_set], [H|Set]) :- sub_set(Sub_set, Set).
+sub_set(Sub_set, [_|Set]) :- sub_set(Sub_set, Set).
+
+pr_subset :-
+  read_str(A, _),
+  sub_set(B, A),
+  write_str(B), nl, fail.
+
+% все сочетания по k элементов
+sochet([], 0, _) :- !.
+sochet([H|Sub_set], K, [H|Set]) :-
+  K1 is K - 1,
+  sochet(Sub_set, K1, Set).
+sochet(Sub_set, K, [_|Set]) :- sochet(Sub_set, K, Set).
+
+pr_sochet :-
+  read_str(A, _),
+  read(K),
+  sochet(B, K, A),
+  write_str(B), nl, fail.
+
+% все сочетания с повторениями
+sochet_p([], 0, _) :- !.
+sochet_p([H|Sub_set], K, [H|Set]) :-
+  K1 is K - 1,
+  sochet_p(Sub_set, K1, [H|Set]).
+sochet_p(Sub_set, K, [_|Set]) :- sochet_p(Sub_set, K, Set).
+
+pr_sochet_p :-
+  read_str(A, _),
+  read(K),
+  sochet_p(B, K, A),
+  write_str(B), nl, fail.
